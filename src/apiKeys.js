52dc659e004ae2a4ef68118dfdd2794f10bb5d0b
Janki-Vallabham <nesojam@gmail.com>
2023-07-07 00:05:20 +0530

    commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f371d64fc51f3d98e2dd22dc56c31540",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
